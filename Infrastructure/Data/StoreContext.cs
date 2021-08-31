8a79e8ee255de41d738992e4292044f50eaad47c
Vinh Tong <Vinh Tong@DESKTOP-2BP5JB6>
2021-08-31 19:33:36 +0700

    End of section 3

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Reflection.PortableExecutable;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,14 @@ namespace Infrastructure.Data
         {
         }
 
-        public DbSet<Product> Products{get;set;}
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
