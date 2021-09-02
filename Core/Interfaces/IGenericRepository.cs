e971de407bce712d3cb05181f147786c9c4de217
Vinh Tong <Vinh Tong@DESKTOP-2BP5JB6>
2021-09-02 11:08:42 +0700

    End of section 4

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Specifications;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> GetEntityWithSpec(ISpecifications<T> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpecifications<T> spec);
+    }
+}
