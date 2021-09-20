84d42a185cc671d90d432188c76082e6848890c9
Vinh Tong <Vinh Tong@DESKTOP-2BP5JB6>
2021-09-20 09:40:28 +0700

    End of Section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './basket/basket.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialized basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
